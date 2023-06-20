7d889d0e27aa5a4539b8dd8af8308d7007d7263d
Charles Cai <caicongchen@gmail.com>
2023-06-19 21:40:08 -0400

    add GraphQL mutation.

--- src/main/java/com/chuwa/redbook/controller/PostGraphQLController.java
+++ src/main/java/com/chuwa/redbook/controller/PostGraphQLController.java
@@ -2,18 +2,32 @@ package com.chuwa.redbook.controller;
 
 import com.chuwa.redbook.payload.PostDto;
 import com.chuwa.redbook.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PostGraphQLController {
-    @Autowired
-    private PostService postService;
+    private final PostService postService;
+
+    public PostGraphQLController(PostService postService) {
+        this.postService = postService;
+    }
 
     @QueryMapping
     public PostDto postById(@Argument Long id){
         return postService.getPostById(id);
+    }
+
+    @MutationMapping
+    public PostDto createPost(@Argument String title, @Argument String description, @Argument String content){
+        PostDto postDto = new PostDto();
+
+        postDto.setTitle(title);
+        postDto.setDescription(description);
+        postDto.setContent(content);
+
+        return postService.createPost(postDto);
     }
 }
