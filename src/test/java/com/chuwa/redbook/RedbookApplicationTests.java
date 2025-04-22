31b12d4bf5a5972c44558f1015f330b93cd69e28
CAnonymous <zidong.yu@outlook.com>
2025-04-21 19:34:30 -0700

    add integration test example.

--- src/test/java/com/chuwa/redbook/RedbookApplicationTests.java
+++ src/test/java/com/chuwa/redbook/RedbookApplicationTests.java
@@ -1,13 +1,33 @@
 package com.chuwa.redbook;
 
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.service.impl.PostServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RedbookApplicationTests {
 
+	@Autowired
+	private PostServiceImpl postService;
+
 	@Test
-	void contextLoads() {
+	void testGetAllPosts() {
+		assertNotNull(postService.getAllPost());
 	}
+
+    @Test
+	void testCreatePost() {
+        PostDto postDto = new PostDto();
+        postDto.setTitle("title");
+        postDto.setContent("content");
+        postDto.setDescription("description");
+        postService.createPost(postDto);
+
+        assertNotNull(postService.getAllPost());
+    }
 
 }
