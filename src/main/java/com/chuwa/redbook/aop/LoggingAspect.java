a7f068923c196638a81ea59d450c58ade5b52b8e
CAnonymous <zidong.yu@outlook.com>
2025-07-16 21:35:41 -0700

    use readable point cut expressions.

--- src/main/java/com/chuwa/redbook/aop/LoggingAspect.java
+++ src/main/java/com/chuwa/redbook/aop/LoggingAspect.java
@@ -24,33 +24,20 @@ public class LoggingAspect {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     /**
-     * Pointcut that matches all repositories, services and Web REST endpoints.
-     */
-    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
-            " || within(@org.springframework.stereotype.Service *)" +
-            " || within(@org.springframework.web.bind.annotation.RestController *)")
-    public void springBeanPointcut() {
-        // Method is empty as this is just a Pointcut, the implementations are in the advices.
-    }
-
-    /**
-     * Pointcut that matches all Spring beans in the application's main packages.
-     */
-    @Pointcut("within(com.chuwa.redbook.security..*)" +
-            " || within(com.chuwa.redbook.service..*)" +
-            " || within(com.chuwa.redbook.controller..*)" +
-            " || within(com.chuwa.redbook.dao..*)")
-    public void applicationPackagePointcut() {
-        // Method is empty as this is just a Pointcut, the implementations are in the advices.
-    }
-
-    /**
      * Advice that logs methods throwing exceptions.
      *
      * @param joinPoint join point for advice
      * @param e         exception
      */
-    @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
+    @AfterThrowing(
+            pointcut = "within(@org.springframework.stereotype.Repository *)" +
+                    " || within(@org.springframework.stereotype.Service *)" +
+                    " || within(@org.springframework.web.bind.annotation.RestController *)" +
+                    " && (within(com.chuwa.redbook.security..*)" +
+                    " || within(com.chuwa.redbook.service..*)" +
+                    " || within(com.chuwa.redbook.controller..*)" +
+                    " || within(com.chuwa.redbook.dao..*))",
+            throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
         log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
@@ -64,7 +51,13 @@ public class LoggingAspect {
      * @return result
      * @throws Throwable throws IllegalArgumentException
      */
-    @Around("applicationPackagePointcut() && springBeanPointcut()")
+    @Around("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)" +
+            " && (within(com.chuwa.redbook.security..*)" +
+            " || within(com.chuwa.redbook.service..*)" +
+            " || within(com.chuwa.redbook.controller..*)" +
+            " || within(com.chuwa.redbook.dao..*))")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         if (log.isDebugEnabled()) {
             log.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
